a464dd0cb411de3b519f7885fae63acffdf637f9
Dennis Fridrich <fridrich.dennis@gmail.com>
2016-03-24 10:12:25 +0100

    Add methods for verificate IC + RC

--- src/Lib.php
+++ src/Lib.php
@@ -1001,4 +1001,88 @@ class Lib
     {
         return !filter_var($email, FILTER_VALIDATE_EMAIL) ? false : true;
     }
+    
+    /**
+     * Overeni rodneho cisla.
+     * 
+     * @see https://phpfashion.com/jak-overit-platne-ic-a-rodne-cislo
+     * @param $rc
+     * @return bool
+     */
+    public static function verifyRC($rc)
+    {
+        // be liberal in what you receive
+        if (!preg_match('#^\s*(\d\d)(\d\d)(\d\d)[ /]*(\d\d\d)(\d?)\s*$#', $rc, $matches)) {
+            return false;
+        }
+
+        list(, $year, $month, $day, $ext, $c) = $matches;
+
+        if ($c === '') {
+            $year += $year < 54 ? 1900 : 1800;
+        } else {
+            // kontrolní číslice
+            $mod = ($year . $month . $day . $ext) % 11;
+            if ($mod === 10) {
+                $mod = 0;
+            }
+            if ($mod !== (int)$c) {
+                return false;
+            }
+
+            $year += $year < 54 ? 2000 : 1900;
+        }
+
+        // k měsíci může být připočteno 20, 50 nebo 70
+        if ($month > 70 && $year > 2003) {
+            $month -= 70;
+        } elseif ($month > 50) {
+            $month -= 50;
+        } elseif ($month > 20 && $year > 2003) {
+            $month -= 20;
+        }
+
+        // kontrola data
+        if (!checkdate($month, $day, $year)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Overeni ICa.
+     * 
+     * @see https://phpfashion.com/jak-overit-platne-ic-a-rodne-cislo
+     * @param $ic
+     * @return bool
+     */
+    public static function verifyIC($ic)
+    {
+        // be liberal in what you receive
+        $ic = preg_replace('#\s+#', '', $ic);
+
+        // má požadovaný tvar?
+        if (!preg_match('#^\d{8}$#', $ic)) {
+            return false;
+        }
+
+        // kontrolní součet
+        $a = 0;
+        for ($i = 0; $i < 7; $i++) {
+            $a += $ic[$i] * (8 - $i);
+        }
+
+        $a = $a % 11;
+        if ($a === 0) {
+            $c = 1;
+        } elseif ($a === 1) {
+            $c = 0;
+        } else {
+            $c = 11 - $a;
+        }
+
+        return (int)$ic[7] === $c;
+    }
+    
 }
