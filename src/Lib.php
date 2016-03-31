2cc269c44755ec287e6415b7714ae9e6e050b260
Dennis Fridrich <fridrich.dennis@gmail.com>
2016-03-31 11:09:15 +0200

    Fix getIp()

--- src/Lib.php
+++ src/Lib.php
@@ -47,7 +47,7 @@ class Lib
         if (isset($_SERVER["HTTP_X_FORWARDED_FOR"]) && $_SERVER["HTTP_X_FORWARDED_FOR"] != "") {
             return $_SERVER["HTTP_X_FORWARDED_FOR"];
         } else {
-            return $_SERVER["REMOTE_ADDR"];
+            return @$_SERVER["REMOTE_ADDR"];
         }
     }
 
